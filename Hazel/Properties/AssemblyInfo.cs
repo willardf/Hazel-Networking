9e39b2ec65ac607be1ca89717afecead6984b827
Forest <forest@innersloth.com>
2020-10-13 22:42:18 -0700

    Cleanup old, unused doc project and fix #7

--- Hazel/Properties/AssemblyInfo.cs
+++ Hazel/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Hazel")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,14 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-// NuGet version information
-[assembly: AssemblyInformationalVersion("0.1.2-beta")]
 
 // Show internals to unit testing assembly so it can test
 [assembly:InternalsVisibleTo("Hazel.UnitTests")]
