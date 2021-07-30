392aacdf1218c01a245351a2005a3521342ab2eb
Matthew Endsley <mendsley@gmail.com>
2021-07-29 22:55:00 -0700

    Add Bcrypt implementation of IAes

---
+++ Hazel.Windows.AesBcrypt/AesBcrypt.cpp
@@ -0,0 +1,151 @@
+#include <stdint.h>
+#include <memory>
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <bcrypt.h>
+#pragma comment(lib, "bcrypt.lib")
+
+#define NT_SUCCESS(status) (static_cast<NTSTATUS>(status) >= 0)
+
+struct AesBcrypt
+{
+	BCRYPT_ALG_HANDLE m_algorithm = nullptr;
+	BCRYPT_KEY_HANDLE m_key = nullptr;
+
+	~AesBcrypt()
+	{
+		if (nullptr != m_key)
+		{
+			BCryptDestroyKey(m_key);
+		}
+
+		if (nullptr != m_algorithm)
+		{
+			BCryptCloseAlgorithmProvider(m_algorithm, 0);
+		}
+	}
+};
+
+/**
+ * Create a new AesBcrypt context with the specified key
+ */
+extern "C" __declspec(dllexport) AesBcrypt* AesBrypt_create(uint8_t* keyArray, int keyArrayOffset, int keyArrayLength)
+{
+	std::unique_ptr<AesBcrypt> context = std::make_unique<AesBcrypt>();
+
+	NTSTATUS result;
+
+	// Validate key size
+	if (keyArrayLength != 16 && keyArrayLength != 24 && keyArrayLength != 32)
+	{
+		return nullptr;
+	}
+
+	// Create the AES algorithm
+	result = BCryptOpenAlgorithmProvider(
+		  &context->m_algorithm
+		, BCRYPT_AES_ALGORITHM
+		, nullptr
+		, 0
+	);
+	if (!NT_SUCCESS(result))
+	{
+		return nullptr;
+	}
+
+	DWORD blockLength = keyArrayLength;
+	result = BCryptSetProperty(
+		  context->m_algorithm
+		, BCRYPT_BLOCK_LENGTH
+		, reinterpret_cast<PBYTE>(&blockLength)
+		, sizeof(blockLength)
+		, 0
+	);
+	if (!NT_SUCCESS(result))
+	{
+		return nullptr;
+	}
+
+	DWORD keyLengthInBits = keyArrayLength * 8;
+	result = BCryptSetProperty(
+		  context->m_algorithm
+		, BCRYPT_KEY_LENGTH
+		, reinterpret_cast<PBYTE>(&keyLengthInBits)
+		, sizeof(keyLengthInBits)
+		, 0
+	);
+	if (!NT_SUCCESS(result))
+	{
+		return nullptr;
+	}
+
+	result = BCryptSetProperty(
+		  context->m_algorithm
+		, BCRYPT_CHAINING_MODE
+		, reinterpret_cast<PBYTE>(const_cast<wchar_t*>(BCRYPT_CHAIN_MODE_ECB))
+		, sizeof(BCRYPT_CHAIN_MODE_ECB)
+		, 0
+	);
+	if (!NT_SUCCESS(result))
+	{
+		return nullptr;
+	}
+
+	// Create the symetric key object
+	result = BCryptGenerateSymmetricKey(
+		  context->m_algorithm
+		, &context->m_key
+		, nullptr
+		, 0
+		, keyArray + keyArrayOffset
+		, keyArrayLength
+		, 0
+	);
+	if (!NT_SUCCESS(result))
+	{
+		return nullptr;
+	}
+
+	return context.release();
+}
+
+/**
+ * Destroy the specified AesBcrypt context
+ */
+extern "C" __declspec(dllexport) void AesBcrypt_release(AesBcrypt * unsafeContext)
+{
+	std::unique_ptr<AesBcrypt> context(unsafeContext);
+	context.reset();
+}
+
+/**
+ * Encrypt a block with the specified AesBcrypt context
+ */
+extern "C" __declspec(dllexport) int AesBcrypt_encrpytBlock(AesBcrypt * context, uint8_t * inputArray, int inputOffset, int inputLength, uint8_t * outputArray, int outputOffset)
+{
+	if (nullptr == context)
+	{
+		return 0;
+	}
+
+	ULONG bytesEncrypted = 0;
+	NTSTATUS result = BCryptEncrypt(
+		  context->m_key
+		, inputArray + inputOffset
+		, inputLength
+		, nullptr
+		, nullptr
+		, 0
+		, outputArray + outputOffset
+		, inputLength
+		, &bytesEncrypted
+		, 0
+	);
+	if (!NT_SUCCESS(result))
+	{
+		return 0;
+	}
+
+	return 0;
+}
---
+++ Hazel/Crypto/BcryptAes.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Hazel.Crypto
+{
+    /// <summary>
+    /// AES implementation using he native Windows Bcrypt API
+    /// </summary>
+    public class BcryptAes : IAes
+    {
+        private IntPtr m_context = IntPtr.Zero;
+
+        [DllImport("Hazel.Aes.Bcrypt")]
+        private static extern IntPtr AesBrypt_create(byte[] keyArray, int keyArrayOffset, int keyArrayLength);
+
+        [DllImport("Hazel.Aes.Bcrypt")]
+        private static extern void AesBcrypt_release(IntPtr context);
+
+        [DllImport("Hazel.Aes.Bcrypt")]
+        private static extern int AesBcrypt_encrpytBlock(IntPtr context, byte[] inputArray, int inputOffset, int inputLength, byte[] outputArray, int outputOffset);
+
+        /// <summary>
+        /// Create a new instance of the Bcrypt AES implementation
+        /// </summary>
+        /// <param name="key">Encryption key</param>
+        public BcryptAes(ByteSpan key)
+        {
+            this.m_context = AesBrypt_create(key.GetUnderlyingArray(), key.Offset, key.Length);
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            AesBcrypt_release(this.m_context);
+            this.m_context = IntPtr.Zero;
+        }
+
+        /// <inheritdoc/>
+        public int EncryptBlock(ByteSpan inputSpan, ByteSpan outputSpan)
+        {
+            if (inputSpan.Length != outputSpan.Length)
+            {
+                throw new ArgumentException($"ouputSpan length ({outputSpan.Length}) does not match inputSpan length ({inputSpan.Length})", nameof(outputSpan));
+            }
+
+            return AesBcrypt_encrpytBlock(this.m_context, inputSpan.GetUnderlyingArray(), inputSpan.Offset, inputSpan.Length, outputSpan.GetUnderlyingArray(), outputSpan.Offset);
+        }
+    }
+}
