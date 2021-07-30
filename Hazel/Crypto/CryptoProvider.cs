1a2f601cd0a7a555dc90cb1374fcab66c56e2f66
Matthew Endsley <mendsley@gmail.com>
2021-07-29 22:23:14 -0700

    Allow the default AES implementation to be overridden

---
+++ Hazel/Crypto/CryptoProvider.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hazel.Crypto
+{
+    public static class CryptoProvider
+    {
+        public delegate IAes CreateAesOverrideDelegate(ByteSpan key);
+
+        /// <summary>
+        /// Override the default AES creation function
+        /// </summary>
+        public static CreateAesOverrideDelegate OverrideCreateAes = null;
+
+        /// <summary>
+        /// Create a new AES cipher
+        /// </summary>
+        /// <param name="key">Encrtyption key</param>
+        public static IAes CreateAes(ByteSpan key)
+        {
+            if (OverrideCreateAes != null)
+            {
+                IAes result = OverrideCreateAes(key);
+                if (null != result)
+                {
+                    return result;
+                }
+            }
+
+            return new DefaultAes(key);
+        }
+    }
+}
---
+++ Hazel/Crypto/DefaultAes.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Hazel.Crypto
+{
+    /// <summary>
+    /// AES provider using the default System.Security.Cryptography implementation
+    /// </summary>
+    public class DefaultAes : IAes
+    {
+        private readonly ICryptoTransform encryptor_;
+
+        /// <summary>
+        /// Create a new default instance of the AES block cipher
+        /// </summary>
+        /// <param name="key">Encryption key</param>
+        public DefaultAes(ByteSpan key)
+        {
+            // Create the AES block cipher
+            using (Aes aes = Aes.Create())
+            {
+                aes.KeySize = key.Length * 8;
+                aes.BlockSize = aes.KeySize;
+                aes.Mode = CipherMode.ECB;
+                aes.Padding = PaddingMode.Zeros;
+                aes.Key = key.ToArray();
+
+                this.encryptor_ = aes.CreateEncryptor();
+            }
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            this.encryptor_.Dispose();
+        }
+
+        /// <inheritdoc/>
+        public int EncryptBlock(ByteSpan inputSpan, ByteSpan outputSpan)
+        {
+            if (inputSpan.Length != outputSpan.Length)
+            {
+                throw new ArgumentException($"ouputSpan length ({outputSpan.Length}) does not match inputSpan length ({inputSpan.Length})", nameof(outputSpan));
+            }
+
+            return this.encryptor_.TransformBlock(inputSpan.GetUnderlyingArray(), inputSpan.Offset, inputSpan.Length, outputSpan.GetUnderlyingArray(), outputSpan.Offset);
+        }
+    }
+}
